70e16015019ff9bb24cd7e3c0a365d23c11118d1
brais <braisfv22@gmail.com>
2026-03-14 13:50:42 +0100

    improve start screen by making favourite items fill full width

--- app/src/main/java/com/braiso22/mylauncher/GreetingScreen.kt
+++ app/src/main/java/com/braiso22/mylauncher/GreetingScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -16,12 +15,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.braiso22.mylauncher.domain.AppRepository
-import com.braiso22.mylauncher.ui.theme.MyLauncherTheme
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.Dispatchers
@@ -124,6 +121,7 @@ fun Greeting(
                                 launchFavoriteApp(context, app)
                             }
                         },
+                        modifier = Modifier.fillMaxWidth()
                     )
                 }
             }
@@ -141,7 +139,7 @@ private fun FavoriteAppItem(
         modifier = modifier
             .clickable(onClick = onClick)
             .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
         Text(
