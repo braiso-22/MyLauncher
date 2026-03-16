8f9b56d725d53ca16731f52f2e3d75ad20208b7e
brais <braisfv22@gmail.com>
2026-03-16 23:39:14 +0100

    updated version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.braiso22.mylauncher"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
